b53f23a24cde3be5ea3f7e64aa3a8b783726b021
liwuhua <1551647727@qq.com>
2019-09-12 14:51:46 +0800

    button响应点击事件

--- LWHTestOne/模块/首页/LWHSwiftViewController.swift
+++ LWHTestOne/模块/首页/LWHSwiftViewController.swift
@@ -13,17 +13,16 @@ class LWHSwiftViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.red
-//        let VC = LWHWalletViewController()
-//        self.navigationController?.pushViewController(VC, animated: true)
+        
         let button = UIButton(type: UIButton.ButtonType.custom)
         button.frame = CGRect.init(x: 0, y: 300, width: 100, height: 40)
         button.backgroundColor = UIColor.blue
         button.titleLabel?.text = "点我"
-        button.target(forAction: Selector.init(("tapAction")), withSender: nil)
+        button.addTarget(self, action: #selector(tapAction), for: UIControl.Event.touchUpInside)
         self.view.addSubview(button)
         
     }
-    func tapAction() {
+    @objc func tapAction() {
         print("你好")
     }
 
