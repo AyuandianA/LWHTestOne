084462b0a18ee4e873a5b3eeb64dfffe5e6844b8
liwuhua <1551647727@qq.com>
2020-01-11 00:24:54 +0800

    2222

--- LWHTestOne/模块/首页/LWHFirstViewController.m
+++ LWHTestOne/模块/首页/LWHFirstViewController.m
@@ -20,34 +20,48 @@
     [super viewDidLoad];
     self.isHiddenReturnButton = YES;
     self.view.backgroundColor = [UIColor whiteColor];
+//    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithActionBlock:^(id  _Nonnull sender) {
+//
+//    }]];
+    
+    
     UIButton *Button = [UIButton buttonWithType:(UIButtonTypeCustom)];
     [Button setTitle:@"Push" forState:(UIControlStateNormal)];
     [Button setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
-    [Button addTarget:self action:@selector(ButtonAction) forControlEvents:(UIControlEventTouchUpInside)];
+//    [Button addTarget:self action:@selector(ButtonAction) forControlEvents:(UIControlEventTouchUpInside)];
     Button.titleLabel.font = [UIFont systemFontOfSize:TextFont];
     Button.layer.masksToBounds = YES;
     Button.layer.cornerRadius = 5;
     Button.backgroundColor = [UIColor redColor];
-    [self.view addSubview:Button];
     Button.width = 100;
     Button.height = 40;
     Button.centerX = self.view.centerX;
     Button.centerY  = self.view.centerY;
+    [self.view addSubview:Button];
     
-//    convertViewToImage
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:Button.frame];
+    imageView.backgroundColor = [UIColor redColor];
+    imageView.userInteractionEnabled = YES;
+    [self.view addSubview:imageView];
+//    [imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithActionBlock:^(id  _Nonnull sender) {
+//
+//    }]];
+    
+    UIView *viewOne = [[UIView alloc]initWithFrame:Button.frame];
+    viewOne.backgroundColor = [UIColor blueColor];
+    viewOne.userInteractionEnabled = YES;
+//    [viewOne addGestureRecognizer: [[UITapGestureRecognizer alloc]initWithActionBlock:^(id  _Nonnull sender) {
+//
+//    }]];
+    [self.view addSubview:viewOne];
 }
--(void)ButtonAction{
-    LWHWalletViewController *VC = [[LWHWalletViewController alloc]init];
-    [self.navigationController pushViewController:VC animated:YES];
-}
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+//-(void)ButtonAction{
+//    LWHWalletViewController *VC = [[LWHWalletViewController alloc]init];
+//    [self.navigationController pushViewController:VC animated:YES];
+//}
+//-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+//{
+//
+//}
 
 @end
