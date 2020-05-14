84f48411900b4870b9cfffde14cd6b41324730e9
liwuhua <1551647727@qq.com>
2020-05-14 22:48:56 +0800

    trtu

--- LWHTestOne/模块/首页/首页控制器/LWHFirstViewController.m
+++ LWHTestOne/模块/首页/首页控制器/LWHFirstViewController.m
@@ -28,7 +28,7 @@
     self.tableView = [[LWHTableViewAutoHeight alloc]initWithFrame:CGRectMake(0, TopHeight + 20, KScreenWidth, 200) style:(UITableViewStylePlain)];
     self.tableView.scrollEnabled = NO;
     self.tableView.backgroundColor = [UIColor redColor];
-    self.tableView.SourceArray  = @[@"公司的法国人同一天",@"r而台湾而对方公司的感受到",@"分公司答复公司法告诉对方公司法规的三个大公司的公司的股份第三个分公司答复公司法告诉对方公司法规的三个大公司的公司的股份第三个分公司答复公司法告诉对方公司法规的三个大公司的公司的股份第三个分公司答复公司法告诉对方公司法规的三个大公司的公司的股份第三个",@"而台湾而对方公司的感受到而台湾而对方公司的感受到",@"而台湾而对方公司的感受到",@"发不发给你个"].mutableCopy;
+    self.tableView.SourceArray  = @[@"公司的法国人同一天",@"r而台湾而对方公司的感受到",@"分公司答复公司法告诉对方公司法规的三个大公司的公司的股份第三个分公司答复公司法告诉对方公司法规的三个大公司的公司的股份第三个分公司答复公司法告诉对方公司法规的三个大公司的公司的股份第三个分公司答复公司法告诉对方公司法规的三个大公司的公司的股份第三个"].mutableCopy;
     [self.tableView reloadData];
     LWHTableViewCellAutoHeight *cell = [[LWHTableViewCellAutoHeight alloc]initWithStyle:(UITableViewCellStyleDefault) reuseIdentifier:@"cell"];
     CGFloat heightAll = 0;
