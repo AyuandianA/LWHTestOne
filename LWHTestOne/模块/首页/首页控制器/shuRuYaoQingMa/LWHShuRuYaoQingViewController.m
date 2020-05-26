2f4ee9019961e234b35ff9b88e595df1a181aae6
liwuhua <1551647727@qq.com>
2020-05-26 18:03:20 +0800

    q'q'q

--- LWHTestOne/模块/首页/首页控制器/shuRuYaoQingMa/LWHShuRuYaoQingViewController.m
+++ LWHTestOne/模块/首页/首页控制器/shuRuYaoQingMa/LWHShuRuYaoQingViewController.m
@@ -22,7 +22,8 @@
     topBackView.backgroundColor = RGB(250, 250, 250, 1);
     [self.view addSubview:topBackView];
     [topBackView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.left.right.equalTo(self.view);
+        make.left.right.equalTo(self.view);
+        make.top.equalTo(self.view).offset(TopHeight);
     }];
     
     YYLabel *yaoQingTop = [[YYLabel alloc]init];
@@ -51,31 +52,13 @@
         make.left.right.equalTo(topBackView).offset(margin);
         make.bottom.equalTo(topBackView).offset(-margin * 2);
     }];
-//    yaoQingBottom.text = @"红富士苹果直播卖货，速来围观 红富士苹果直播卖货";
-//
-//    UITextField *leftField = [[UITextField alloc]init];
-//    leftLable.font = [UIFont systemFontOfSize:TextFont - 2 weight:-0.3];
-//    leftLable.textVerticalAlignment = YYTextVerticalAlignmentCenter;
-//    leftLable.textAlignment = NSTextAlignmentCenter;
-//    leftLable.textColor = [UIColor blackColor];
-//    [self.view addSubview:leftLable];
-//    [yaoQingBottom mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.top.equalTo(yaoQingTop.mas_bottom).offset(margin * 2);
-//        make.left.right.equalTo(topBackView).offset(margin);
-//        make.bottom.equalTo(topBackView).offset(-margin * 2);
-//    }];
-//    yaoQingBottom.text = @"红富士苹果直播卖货，速来围观 红富士苹果直播卖货";
+    yaoQingBottom.text = @"红富士苹果直播卖货，速来围观 红富士苹果直播卖货";
+    
+    
+    
     
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
